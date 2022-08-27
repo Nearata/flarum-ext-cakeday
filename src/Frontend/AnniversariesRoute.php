4e5c7a4adfaec14f41776dabd2cc9b6400c7aa9d
William <nearata@protonmail.com>
2022-08-27 15:12:46 +0200

    Fix document title translation

--- src/Frontend/AnniversariesRoute.php
+++ src/Frontend/AnniversariesRoute.php
@@ -5,10 +5,18 @@ namespace Nearata\CakeDay\Frontend;
 use Flarum\Frontend\Document;
 use Flarum\Http\Exception\RouteNotFoundException;
 use Flarum\Http\RequestUtil;
+use Flarum\Locale\Translator;
 use Psr\Http\Message\ServerRequestInterface;
 
 class AnniversariesRoute
 {
+    protected $translator;
+
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function __invoke(Document $document, ServerRequestInterface $request)
     {
         $actor = RequestUtil::getActor($request);
@@ -17,6 +25,6 @@ class AnniversariesRoute
             throw new RouteNotFoundException();
         }
 
-        $document->title = 'Anniversaries';
+        $document->title = $this->translator->get("nearata-cakeday.forum.page.title");
     }
 }
